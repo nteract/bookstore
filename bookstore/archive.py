eae3a0b1f5f59af1ab7477684bee62bacd4cd6dc
M Pacer <mpacer@berkeley.edu>
2018-11-12 14:21:56 -0800

    use configurable objects from within BookstoreContentsArchiver

--- bookstore/archive.py
+++ bookstore/archive.py
@@ -1,5 +1,11 @@
-from s3contents.ipycompat import ContentsManager
-from s3contents.ipycompat import HasTraits, Unicode
+from notebook.services.contents.filemanager import FileContentsManager
+from traitlets import Unicode
+from pprint import pprint
+
+import nbformat 
+
+from .bookstore_config import Bookstore
+
 
 from traitlets import (
     Any,
@@ -14,7 +20,8 @@ from traitlets import (
     default,
 )
 
-class BookstoreContentsArchiver(ContentsManager, HasTraits):
+
+class BookstoreContentsArchiver(FileContentsManager):
     """
     Archives contents via one ContentsManager and passes through to
     another ContentsManager.
@@ -24,4 +31,21 @@ class BookstoreContentsArchiver(ContentsManager, HasTraits):
     * Write directly to S3 on post_save_hook
 
     """
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.bookstore = Bookstore(parent=self)
+    
+    def run_pre_save_hook(self, model, path, **kwargs):
+        """Override the direct pre_save_hook to implement our own without monkey patching
+        """
+
+        if model['type'] != 'notebook':
+            return
+
+        notebook = nbformat.from_dict(model['content'])
+
+        # write to S3 asynchronously
+        # store the hash
+
+        print(notebook)
