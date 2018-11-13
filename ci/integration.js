9ecb8bd7a912fa9d45c1048c9cfef761249fb70a
Kyle Kelley <rgbkrk@gmail.com>
2018-11-13 10:33:14 -0800

    refactor integration script into separate modules

--- ci/integration.js
+++ ci/integration.js
@@ -1,50 +1,27 @@
-const child_process = require("child_process");
-const { randomBytes } = require("crypto");
 const path = require("path");
 const fs = require("fs");
 
-global.XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-const { ajax } = require("rxjs/ajax");
-
-const rxJupyter = require("rx-jupyter");
-
 const _ = require("lodash");
 
-console.log("running bookstore integration tests");
+const s3 = require("./s3");
+const { JupyterServer } = require("./jupyter");
 
-async function genToken(byteLength = 32) {
-  return new Promise((resolve, reject) => {
-    randomBytes(byteLength, (err, buffer) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      resolve(buffer.toString("hex"));
-    });
-  });
-}
-
-const sleep = timeout =>
-  new Promise((resolve, reject) => setTimeout(resolve, timeout));
+const { sleep } = require("./sleep");
 
 // Catch all rogue promise rejections to fail CI
 process.on("unhandledRejection", error => {
   console.log("unhandledRejection", error.message);
+  console.error(error.stack);
   process.exit(2);
 });
 
-var Minio = require("minio");
+console.log("running bookstore integration tests");
 
 const main = async () => {
-  const jupyterToken = await genToken();
-  const jupyterPort = 9988;
-  const jupyterEndpoint = `http://127.0.0.1:${jupyterPort}`;
-
   const bucketName = "bookstore";
-  // Optional according to minio docs, likely here for AWS compat
-  const regionName = "us-east-1";
+
+  const jupyterServer = new JupyterServer();
+  await jupyterServer.start();
 
   const s3Config = {
     endPoint: "127.0.0.1",
@@ -56,69 +33,11 @@ const main = async () => {
 
   // Instantiate the minio client with the endpoint
   // and access keys as shown below.
-  var minioClient = new Minio.Client(s3Config);
+  var s3Client = new s3.Client(s3Config);
 
-  const madeBucket = await new Promise((resolve, reject) => {
-    minioClient.makeBucket(bucketName, regionName, err => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve();
-    });
-  });
+  await s3Client.makeBucket(bucketName);
+
   console.log(`Created bucket ${bucketName}`);
-
-  const jupyter = child_process.spawn(
-    "jupyter",
-    [
-      "notebook",
-      "--no-browser",
-      `--NotebookApp.token=${jupyterToken}`,
-      `--NotebookApp.disable_check_xsrf=True`,
-      `--port=${jupyterPort}`,
-      `--ip=127.0.0.1`
-    ],
-    { cwd: __dirname }
-  );
-
-  ////// Refactor me later, streams are a bit messy with async await
-  // Check to see that jupyter is up
-  let jupyterUp = false;
-
-  jupyter.stdout.on("data", data => {
-    const s = data.toString();
-    console.log(s);
-  });
-  jupyter.stderr.on("data", data => {
-    const s = data.toString();
-
-    console.error(s);
-    if (s.includes("Jupyter Notebook is running at")) {
-      jupyterUp = true;
-    }
-  });
-  jupyter.stdout.on("end", data => console.log("DONE WITH JUPYTER"));
-
-  jupyter.on("exit", code => {
-    if (code != 0) {
-      // Jupyter exited badly
-      console.error("jupyter errored", code);
-      process.exit(code);
-    }
-  });
-
-  await sleep(3000);
-
-  if (!jupyterUp) {
-    console.log("jupyter has not come up after 3 seconds, waiting 3 more");
-    await sleep(3000);
-
-    if (!jupyterUp) {
-      console.log("jupyter has not come up after 6 seconds, bailing");
-      process.exit(1);
-    }
-  }
 
   const originalNotebook = {
     cells: [
@@ -153,47 +72,18 @@ const main = async () => {
     nbformat_minor: 2
   };
 
-  const xhr = await ajax({
-    url: `${jupyterEndpoint}/api/contents/ci-local-writeout.ipynb`,
-    responseType: "json",
-    createXHR: () => new XMLHttpRequest(),
-    method: "PUT",
-    body: {
-      type: "notebook",
-      content: originalNotebook
-    },
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `token ${jupyterToken}`
-    }
-  }).toPromise();
+  jupyterServer.writeNotebook("ci-local-writeout.ipynb", originalNotebook);
 
   // Wait for minio to have the notebook
   // Future iterations of this script should poll to get the notebook
   await sleep(1000);
 
-  jupyter.kill();
+  jupyterServer.shutdown();
 
-  //// Check the notebook we placed on S3
-  const rawNotebook = await new Promise((resolve, reject) =>
-    minioClient.getObject(
-      bucketName,
-      "ci-workspace/ci-local-writeout.ipynb",
-      (err, dataStream) => {
-        if (err) {
-          console.error("wat");
-          reject(err);
-          return;
-        }
-
-        const chunks = [];
-        dataStream.on("data", chunk => chunks.push(chunk));
-        dataStream.on("error", reject);
-        dataStream.on("end", () => {
-          resolve(Buffer.concat(chunks).toString("utf8"));
-        });
-      }
-    )
+  /***** Check notebook from S3 *****/
+  const rawNotebook = await s3Client.getObject(
+    bucketName,
+    "ci-workspace/ci-local-writeout.ipynb"
   );
 
   const notebook = JSON.parse(rawNotebook);
@@ -208,7 +98,7 @@ const main = async () => {
 
   console.log("Notebook on S3 matches what we sent");
 
-  //// Check the notebook we placed on Disk
+  /***** Check notebook from Disk *****/
   const diskNotebook = await new Promise((resolve, reject) =>
     fs.readFile(
       path.join(__dirname, "ci-local-writeout.ipynb"),
---
+++ ci/s3.js
@@ -0,0 +1,52 @@
+// Optional according to minio docs, included for AWS compat
+const regionName = "us-east-1";
+
+const Minio = require("minio");
+
+function makeBucket(minioClient, bucketName) {
+  return new Promise((resolve, reject) => {
+    minioClient.makeBucket(bucketName, regionName, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+function getObject(minioClient, bucketName, objectName) {
+  return new Promise((resolve, reject) =>
+    minioClient.getObject(bucketName, objectName, (err, dataStream) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      const chunks = [];
+      dataStream.on("data", chunk => chunks.push(chunk));
+      dataStream.on("error", reject);
+      dataStream.on("end", () => {
+        resolve(Buffer.concat(chunks).toString("utf8"));
+      });
+    })
+  );
+}
+
+class Client {
+  constructor(s3Config) {
+    this.minioClient = new Minio.Client(s3Config);
+  }
+
+  makeBucket(bucketName) {
+    return makeBucket(this.minioClient, bucketName);
+  }
+
+  getObject(bucketName, objectName) {
+    return getObject(this.minioClient, bucketName, objectName);
+  }
+}
+
+module.exports = {
+  Client
+};
