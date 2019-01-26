fa183d0dab3a64beff1af94886942830b61a9bfa
Carol Willing <carolcode@willingconsulting.com>
2019-01-26 12:48:04 -0800

    clean up _version.py

--- bookstore/__init__.py
+++ bookstore/__init__.py
@@ -1,19 +1,12 @@
+from .archive import BookstoreContentsArchiver
+from .bookstore_config import BookstoreSettings
+from .handlers import load_jupyter_server_extension
 from ._version import get_versions
+
 
 __version__ = get_versions()['version']
 del get_versions
-
-from .handlers import load_jupyter_server_extension
 
 
 def _jupyter_server_extension_paths():
     return [dict(module="bookstore")]
-
-
-from .archive import BookstoreContentsArchiver
-
-from .bookstore_config import BookstoreSettings
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
