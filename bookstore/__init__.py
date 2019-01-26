c439806346c41b8d7f87af077a3aba40c09a8717
Carol Willing <carolcode@willingconsulting.com>
2019-01-26 14:59:59 -0800

    comment out git attributes

--- bookstore/__init__.py
+++ bookstore/__init__.py
@@ -10,7 +10,3 @@ del get_versions
 
 def _jupyter_server_extension_paths():
     return [dict(module="bookstore")]
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
