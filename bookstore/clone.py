b845e76b6bb0fb58dd6a680b7541ff1fdb5de546
M Pacer <mpacer@berkeley.edu>
2019-02-26 11:08:42 -0800

    switch handler to get and pass in key as path
    - this works if you use 
    ```
    jupyter notebook --NotebookApp.allow_origin="*" 
    --NotebookApp.disable_check_xsrf=True 
    --BookstoreSettings.s3_bucket=nteract-notebooks
    ```
    
    and 
    
    `python3 ci/publish_request.py`

--- bookstore/clone.py
+++ bookstore/clone.py
@@ -13,7 +13,46 @@ from .bookstore_config import BookstoreSettings
 class BookstoreCloneHandler(APIHandler):
     """Clone a notebook from s3."""
 
-    def post(self, path=''):
+    def initialize(self):
+        # create an easy helper to get at our bookstore settings quickly
+        self.bookstore_settings = BookstoreSettings(config=self.config)
+
+        self.session = aiobotocore.get_session()
+
+    async def _clone(self, path):
+        full_s3_path = s3_path(
+            self.bookstore_settings.s3_bucket, path
+        )
+        file_key = path
+
+        async with self.session.create_client(
+            's3',
+            aws_secret_access_key=self.bookstore_settings.s3_secret_access_key,
+            aws_access_key_id=self.bookstore_settings.s3_access_key_id,
+            endpoint_url=self.bookstore_settings.s3_endpoint_url,
+            region_name=self.bookstore_settings.s3_region_name,
+        ) as client:
+            self.log.info("Processing published write of %s", path)
+            obj = await client.get_object(
+                Bucket=self.bookstore_settings.s3_bucket, Key=file_key
+            )
+            content = await obj['Body'].read()
+            self.log.info("Done with published write of %s", path)
+        
+        self.set_status(201)
+        resp_content = {
+            "s3path": full_s3_path,
+            "content": content.decode('utf-8')
+        }
+        
+        self.log.info(obj)
+        if 'VersionId' in obj:
+            resp_content["versionID"] = obj['VersionId']
+        
+        resp_str = json.dumps(resp_content)
+        self.finish(resp_str)
+
+    async def get(self, path=''):
         """Clone a notebook to a given path.
         
         The payload for this should match that of the contents API for POST.
@@ -26,8 +65,7 @@ class BookstoreCloneHandler(APIHandler):
 
         if path == '' or path == '/':
             raise web.HTTPError(400, "Must have a path to publish to")
+        await self._clone(path.lstrip('/'))
 
-        model = self.get_json_body()
         
-        self.log.info(model)
 
