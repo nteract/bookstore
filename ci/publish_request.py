17f6c27a601d68490d4cf65a0c2ddad0cfc7e206
M Pacer <mpacer@berkeley.edu>
2019-02-26 11:08:42 -0800

    switch handler to get and pass in key as path
    - this works if you use 
    ```
    jupyter notebook --NotebookApp.allow_origin="*" 
    --NotebookApp.disable_check_xsrf=True 
    --BookstoreSettings.s3_bucket=nteract-notebooks
    ```
    
    and 
    
    `python3 ci/publish_request.py`

--- ci/publish_request.py
+++ ci/publish_request.py
@@ -1,11 +1,10 @@
 import requests
 import nbformat 
-# from nbformat.v4 import new_notebook
 
-def main():
-    nb = nbformat.v4.new_notebook()
-    nb.cells.append(nbformat.v4.new_code_cell(source="Hello"))
-    requests.post("http://localhost:8888/api/bookstore/clone/pathy", json=nbformat.writes(nb))
+def main(nb):
+    return requests.get(f"http://localhost:8888/api/bookstore/clone/{nb}")
+
 
 if __name__ == "__main__":
-    main()
+    res = main("published/whateverwewant.json")
+    print(res.json())
