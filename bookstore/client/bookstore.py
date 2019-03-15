cda4ddea0ee0db6f64255ef3d8145f67b1d860bc
M Pacer <mpacer@berkeley.edu>
2019-03-15 05:14:47 -0700

    add APIs for publishing from inside notebook

---
+++ bookstore/client/bookstore.py
@@ -0,0 +1,20 @@
+import requests
+
+from .notebook import CurrentNotebookClient
+
+
+class BookstoreClient(CurrentNotebookClient):
+    @property
+    def publish_endpoint(self):
+        api_endpoint = "/api/bookstore/published/"
+        return f"{self.url}{api_endpoint}"
+
+    def publish(self):
+        nb_json = self.get_contents(self.notebook['path'])['content']
+        req = {
+            "json": {"type": "notebook", "content": nb_json},
+            "headers": {"Content-Type": "application/json"},
+        }
+        target_url = f"{self.publish_endpoint}{self.notebook['path']}"
+
+        resp = requests.put(target_url, **req)
