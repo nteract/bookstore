cf015aced1e89374344da74f77e2dad5d2b87ac4
Carol Willing <carolcode@willingconsulting.com>
2019-03-22 17:05:07 -0700

    refine comment

--- bookstore/_version.py
+++ bookstore/_version.py
@@ -1,17 +1,17 @@
-"""Bookstore version info"""
+"""Bookstore version info
 
-version_info = (
-    2,
-    3,
-    0,
-    "",  # release (b1, rc1, or "" for final or dev)
-    "dev",  # dev or nothing
-)
+Follows pep 440 version rules
 
-# pep 440 version rules: no dot before alpha/beta/rc, but before .dev
-# 0.1.0rc1
-# 0.1.0a1
-# 0.1.0b1.dev
-# 0.1.0.dev
+No dot before alpha/beta/rc, but before .dev
+0.1.0rc1
+0.1.0a1
+0.1.0b1.dev
+0.1.0.dev
+
+version info type of release (b1, rc1, or "" for final or dev)
+dev or "" to designate a final version
+"""
+
+version_info = (2, 3, 0, "", "dev",)
 
 __version__ = ".".join(map(str, version_info[:3])) + ".".join(version_info[3:])
