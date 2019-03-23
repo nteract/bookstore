32483c16ea7a7bc625bffc91d1ca62368530a457
Carol Willing <carolcode@willingconsulting.com>
2019-03-23 12:52:10 -0700

    tidy up file docstring

--- bookstore/_version.py
+++ bookstore/_version.py
@@ -1,15 +1,21 @@
 """Bookstore version info
 
-Follows pep 440 version rules
+Use pep 440 version rules.
 
-No dot before alpha/beta/rc, but before .dev
-0.1.0rc1
-0.1.0a1
-0.1.0b1.dev
-0.1.0.dev
+No dot before alpha/beta/rc. Use dot before `.dev`. Examples::
 
-version info type of release (b1, rc1, or "" for final or dev)
-dev or "" to designate a final version
+- 0.1.0rc1
+- 0.1.0a1
+- 0.1.0b1.dev
+- 0.1.0.dev
+
+`version_info` tuple::
+
+- major
+- minor
+- micro
+- type of release (b1, rc1, or "" for final or dev)
+- suffix (dev or "" to designate a final version)
 """
 
 version_info = (2, 3, 0, "", "dev")
