d38766da72268e3e6a0e180e7a14c1fac39abbe1
Carol Willing <carolcode@willingconsulting.com>
2019-05-10 12:19:22 -0700

    remove commented out lines and expand resp to reponse

--- ci/clone_request.py
+++ ci/clone_request.py
@@ -1,18 +1,20 @@
-import requests
-import nbformat
 import pprint
+
+import nbformat
+import requests
+
 
 '''
 This is intended to be used to interactively debug the bookstore cloning endpoint.
 
-If you want to test this, you'll need to separately run a Jupyter server. 
+If you want to test this, you'll need to separately run a Jupyter server.
 
 Depending on what you want to test you'll need to run different commands
 
 To test the 'nteract-notebooks' bucket on S3 you'll need to run a command like the following
 jupyter notebook --NotebookApp.allow_origin="*" --NotebookApp.disable_check_xsrf=True \
-    --no-browser --NotebookApp.token="" \ 
-    --BookstoreSettings.s3_bucket=nteract-notebooks 
+    --no-browser --NotebookApp.token="" \
+    --BookstoreSettings.s3_bucket=nteract-notebooks
 
 To test the 'bookstore' bucket created with minio you'll need to run a command like the following
 jupyter notebook --NotebookApp.allow_origin="*" --NotebookApp.disable_check_xsrf=True \
@@ -33,24 +35,11 @@ def post(**kwargs):
 
 if __name__ == "__main__":
 
-    # tests s3 hosted 'nteract-notebooks' bucket
-    # res = get("?s3_bucket=nteract-notebooks&s3_key=published/whateverwewant.json")
-    # print(res.content)
-    # res = get("/?s3_bucket=nteract-notebooks&s3_key=published/whateverwewant.json")
-    # print(res.content)
-    # res = get("?s3_bucket=nteract-notebooks&s3_key=Introduction_to_Chainer.ipynb")
-    # print(res.content)
-
-    # tests minio created 'bookstore' bucket
-    # res = get("?s3_bucket=bookstore&s3_key=ci-published/ci-published.ipynb")
-    # print(res.content)
-    # res = post(s3_bucket="nteract-notebooks", s3_key="Introduction_to_Chainer.ipynb")
-    # print(res.content)
-    res = post(s3_bucket="bookstore", s3_key="ci-published/ci-published.ipynb")
-    pprint.pprint(res.json())
-    res = post(
+    response = post(s3_bucket="bookstore", s3_key="ci-published/ci-published.ipynb")
+    pprint.pprint(response.json())
+    response = post(
         s3_bucket="bookstore",
         s3_key="ci-published/ci-published.ipynb",
         target_path="published/this_is_my_target_path.ipynb",
     )
-    pprint.pprint(res.json())
+    pprint.pprint(response.json())
