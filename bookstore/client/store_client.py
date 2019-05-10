efc55b6f518b8e3e7ce14f91d999ef0318e6ca66
Carol Willing <carolcode@willingconsulting.com>
2019-05-10 11:49:55 -0700

    review bookstore client

--- bookstore/client/store_client.py
+++ bookstore/client/store_client.py
@@ -7,13 +7,14 @@ from .nb_client import CurrentNotebookClient
 class BookstoreClient(CurrentNotebookClient):
     """Represents a bookstore client that corresponds to the active nb client"""
 
-    def __init__(self, s3_bucket=""):
+    def __init__(self, s3_bucket=None):
         if s3_bucket:
             self.default_bucket = s3_bucket
         super().__init__()
 
     @property
     def publish_endpoint(self):
+        # TODO: refactor to /api/bookstore/publish/ ?
         api_endpoint = "/api/bookstore/published/"
         return f"{self.url}{api_endpoint}"
 
@@ -21,23 +22,23 @@ class BookstoreClient(CurrentNotebookClient):
         """Publish notebook to bookstore"""
         if path is None:
             path = self.session.path
-        nb_json = self.get_contents(self.session.path)['content']
+        nb_json = self.get_contents(path)['content']
         json_body = {"type": "notebook", "content": nb_json}
 
-        target_url = f"{self.publish_endpoint}{self.session.path}"
+        target_url = f"{self.publish_endpoint}{path}"
 
-        resp = self.req_session.put(target_url, json=json_body)
-        return resp
+        response = self.req_session.put(target_url, json=json_body)
+        return response
 
     @property
     def clone_endpoint(self):
+        # TODO: refactor to /api/bookstore/clone/ ?
         api_endpoint = "/api/bookstore/cloned/"
         return f"{self.url}{api_endpoint}"
 
     def clone(self, s3_bucket="", s3_key="", target_path=""):
         s3_bucket = s3_bucket or self.default_bucket
         json_body = {"s3_bucket": s3_bucket, "s3_key": s3_key, "target_path": target_path}
-        target_url = f"{self.clone_endpoint}"
         # TODO: Add a check for success
-        resp = self.req_session.post(target_url, json=json_body)
-        return resp
+        response = self.req_session.post(f"{self.clone_endpoint}", json=json_body)
+        return response
