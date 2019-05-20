bcf173e906a4d1f0173078b6a1f54e94e654b622
M Pacer <mpacer@berkeley.edu>
2019-05-19 22:43:35 -0700

    Add real url_path_join code from notebook to local utils module
    Code obtained from from 
    https://github.com/jupyter/notebook/blob/9425250005d46c9fb0057b0c322e16d07125b003/notebook/utils.py#L48-L61

--- bookstore/utils.py
+++ bookstore/utils.py
@@ -1,1 +1,16 @@
-from notebook.utils import url_path_join
+def url_path_join(*pieces):
+    """Join components of url into a relative url
+    Use to prevent double slash when joining subpath. This will leave the
+    initial and final / in place
+    """
+    initial = pieces[0].startswith('/')
+    final = pieces[-1].endswith('/')
+    stripped = [s.strip('/') for s in pieces]
+    result = '/'.join(s for s in stripped if s)
+    if initial:
+        result = '/' + result
+    if final:
+        result = result + '/'
+    if result == '//':
+        result = '/'
+    return result
