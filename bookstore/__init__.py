1f2472f4c29388cdf61573ed8433206252a0f351
Carol Willing <carolcode@willingconsulting.com>
2019-05-30 08:45:59 -0700

    remove todo on package dir and make consistent with master

--- bookstore/__init__.py
+++ bookstore/__init__.py
@@ -1,14 +1,14 @@
-# TODO: refactor the import, PACKAGE_DIR, del
 import os
-
-from ._version import __version__, version_info
-from .archive import BookstoreContentsArchiver
-from .bookstore_config import BookstoreSettings
-from .handlers import load_jupyter_server_extension
 
 PACKAGE_DIR: str = os.path.realpath(os.path.dirname(__file__))
 
 del os
+
+from .archive import BookstoreContentsArchiver
+from .bookstore_config import BookstoreSettings
+from .handlers import load_jupyter_server_extension
+from ._version import __version__
+from ._version import version_info
 
 
 def _jupyter_server_extension_paths():
