17260986a5484627fbc2aea989428c01cf1f03b5
M Pacer <mpacer@berkeley.edu>
2019-06-07 15:01:11 -0700

    Update python mocked handler test urls for the BookstoreCloneHandler

--- bookstore/tests/test_clone.py
+++ bookstore/tests/test_clone.py
@@ -32,36 +32,36 @@ class TestCloneHandler(AsyncTestCase):
 
     @gen_test
     async def test_get_no_param(self):
-        empty_handler = self.get_handler('/api/bookstore/cloned')
+        empty_handler = self.get_handler('/bookstore/clone')
         with pytest.raises(HTTPError):
             await empty_handler.get()
 
     @gen_test
     async def test_get_no_bucket(self):
-        no_bucket_handler = self.get_handler('/api/bookstore/cloned?s3_bucket=&s3_key=hi')
+        no_bucket_handler = self.get_handler('/bookstore/clone?s3_bucket=&s3_key=hi')
         with pytest.raises(HTTPError):
             await no_bucket_handler.get()
 
     @gen_test
     async def test_get_no_object_key(self):
-        no_object_key_handler = self.get_handler('/api/bookstore/cloned?s3_bucket=hello&s3_key=')
+        no_object_key_handler = self.get_handler('/bookstore/clone?s3_bucket=hello&s3_key=')
         with pytest.raises(HTTPError):
             await no_object_key_handler.get()
 
     @gen_test
     async def test_get_success(self):
-        success_handler = self.get_handler('/api/bookstore/cloned?s3_bucket=hello&s3_key=my_key')
+        success_handler = self.get_handler('/bookstore/clone?s3_bucket=hello&s3_key=my_key')
         await success_handler.get()
 
     def test_gen_template_params(self):
-        success_handler = self.get_handler('/api/bookstore/cloned?s3_bucket=hello&s3_key=my_key')
+        success_handler = self.get_handler('/bookstore/clone?s3_bucket=hello&s3_key=my_key')
         expected = {
             's3_bucket': 'hello',
             's3_key': 'my_key',
-            'clone_api_url': 'https://localhost:8888/api/bookstore/cloned',
+            'clone_api_url': 'https://localhost:8888/bookstore/clone',
             'redirect_contents_url': 'https://localhost:8888',
         }
-        success_handler = self.get_handler('/api/bookstore/cloned?s3_bucket=hello&s3_key=my_key')
+        success_handler = self.get_handler('/bookstore/clone?s3_bucket=hello&s3_key=my_key')
         output = success_handler.construct_template_params(
             s3_bucket="hello", s3_object_key="my_key"
         )
@@ -72,7 +72,7 @@ class TestCloneHandler(AsyncTestCase):
         expected = {
             's3_bucket': 'hello',
             's3_key': 'my_key',
-            'clone_api_url': 'https://localhost:8888/my_base_url/api/bookstore/cloned',
+            'clone_api_url': 'https://localhost:8888/my_base_url/bookstore/clone',
             'redirect_contents_url': 'https://localhost:8888',
         }
         for base_url in base_url_list:
@@ -84,7 +84,7 @@ class TestCloneHandler(AsyncTestCase):
             )
 
             success_handler = self.get_handler(
-                '/api/bookstore/cloned?s3_bucket=hello&s3_key=my_key', app=mock_app
+                '/api/bookstore/clone?s3_bucket=hello&s3_key=my_key', app=mock_app
             )
             output = success_handler.construct_template_params(
                 s3_bucket="hello", s3_object_key="my_key"
