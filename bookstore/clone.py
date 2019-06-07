069b4cc8058cb7777bf6ec15a64c2f65e1785927
M Pacer <mpacer@berkeley.edu>
2019-06-07 14:01:07 -0700

    Remove POST and S3 cloning logic from the BookstoreCloneHandler
    - now BookstoreCloneHandler only responds with text/html

--- bookstore/clone.py
+++ bookstore/clone.py
@@ -44,40 +44,6 @@ class BookstoreCloneHandler(IPythonHandler):
         """Helper to retrieve bookstore setting for the session."""
         self.bookstore_settings = BookstoreSettings(config=self.config)
 
-        self.session = aiobotocore.get_session()
-
-    async def _clone(self, s3_bucket, file_key):
-        path = file_key
-
-        self.log.info(f"bucket: {s3_bucket}")
-        self.log.info(f"key: {file_key}")
-        full_s3_path = s3_path(s3_bucket, path)
-
-        async with self.session.create_client(
-            's3',
-            aws_secret_access_key=self.bookstore_settings.s3_secret_access_key,
-            aws_access_key_id=self.bookstore_settings.s3_access_key_id,
-            endpoint_url=self.bookstore_settings.s3_endpoint_url,
-            region_name=self.bookstore_settings.s3_region_name,
-        ) as client:
-            self.log.info("Processing published write of %s", path)
-            obj = await client.get_object(Bucket=s3_bucket, Key=file_key)
-            content = await obj['Body'].read()
-            self.log.info("Done with published write of %s", path)
-
-        resp_content = {"s3_path": full_s3_path}
-        model = {
-            "type": "file",
-            "format": "text",
-            "mimetype": "text/plain",
-            "content": content.decode('utf-8'),
-        }
-        self.set_status(201)
-
-        if 'VersionId' in obj:
-            resp_content["versionID"] = obj['VersionId']
-        return model
-
     @web.authenticated
     async def get(self):
         """GET /api/bookstore/cloned
@@ -112,41 +78,6 @@ class BookstoreCloneHandler(IPythonHandler):
             "redirect_contents_url": redirect_contents_url,
         }
         return template_params
-
-    @web.authenticated
-    async def post(self):
-        """POST /api/bookstore/cloned
-
-        Clone a notebook to the path specified in the payload.
-
-        The payload type for the request should be::
-
-            {
-            "s3_bucket": string,
-            "s3_key": string,
-            "target_path"?: string
-            }
-
-        The response payload should match the standard Jupyter contents
-        API POST response.
-        """
-        model = self.get_json_body()
-        s3_bucket = model.get("s3_bucket", "")
-        # s3_paths module has an s3_key function; file_key avoids confusion
-        file_key = model.get("s3_key", "")
-        target_path = model.get("target_path", "") or os.path.basename(os.path.relpath(file_key))
-
-        self.log.info("About to clone from %s", file_key)
-
-        if file_key == '' or file_key == '/':
-            raise web.HTTPError(400, "Must have an S3 key to perform clone.")
-        model = await self._clone(s3_bucket, file_key)
-        self.set_header('Content-Type', 'application/json')
-        path = self.contents_manager.increment_filename(target_path)
-        model['name'] = os.path.basename(os.path.relpath(path))
-        model['path'] = os.path.relpath(path)
-        self.contents_manager.save(model, path)
-        self.finish(model)
 
     def get_template(self, name):
         """Loads a Jinja template by name."""
