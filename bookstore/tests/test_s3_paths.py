9c7e1e02b1059bfae703d1a84069d427906dffdd
Carol Willing <carolcode@willingconsulting.com>
2019-06-08 13:27:54 -0700

    add tests of s3 key

--- bookstore/tests/test_s3_paths.py
+++ bookstore/tests/test_s3_paths.py
@@ -1,7 +1,7 @@
 """Tests for s3 paths"""
 import pytest
 
-from bookstore.s3_paths import s3_display_path
+from bookstore.s3_paths import s3_display_path, s3_key
 
 
 def test_s3_paths():
@@ -24,5 +24,18 @@ def test_s3_paths_no_prefix():
         s3_display_path(bucket, prefix, path)
 
 
-def test_join():
-    pass
+def test_s3_key_no_prefix():
+    with pytest.raises(TypeError):
+        s3_key(path='s3://mybucket')
+
+
+def test_s3_key_invalid_prefix():
+    prefix = 1234
+    with pytest.raises(AttributeError):
+        s3_key(prefix)
+
+
+def test_s3_key_valid_parameters():
+    prefix = 'workspace'
+    path = 'project/manufacturing'
+    assert s3_key(prefix, path) == 'workspace/project/manufacturing'
