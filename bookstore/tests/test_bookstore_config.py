c3a88b550f34a618f547b40f60b2750f423dfb32
M Pacer <mpacer@berkeley.edu>
2019-06-11 09:56:28 -0700

    Cleanup bookstore validation tests

--- bookstore/tests/test_bookstore_config.py
+++ bookstore/tests/test_bookstore_config.py
@@ -6,31 +6,34 @@ from bookstore.bookstore_config import BookstoreSettings, validate_bookstore
 
 def test_validate_bookstore_defaults():
     """Tests that all bookstore validates with default values."""
+    expected = {"bookstore_valid": False, "publish_valid": True, "archive_valid": True}
     settings = BookstoreSettings()
-    assert not validate_bookstore(settings)['bookstore_valid']
-    assert validate_bookstore(settings)['publish_valid']
-    assert validate_bookstore(settings)['archive_valid']
+    assert validate_bookstore(settings) == expected
 
 
 def test_validate_bookstore_published():
     """Tests that bookstore does not validate with an empty published_prefix."""
+    expected = {"bookstore_valid": False, "publish_valid": False, "archive_valid": True}
     settings = BookstoreSettings(published_prefix="")
-    assert not validate_bookstore(settings)['publish_valid']
+    assert validate_bookstore(settings) == expected
 
 
 def test_validate_bookstore_workspace():
     """Tests that bookstore does not validate with an empty workspace_prefix."""
+    expected = {"bookstore_valid": False, "publish_valid": True, "archive_valid": False}
     settings = BookstoreSettings(workspace_prefix="")
-    assert not validate_bookstore(settings)['archive_valid']
+    assert validate_bookstore(settings) == expected
 
 
 def test_validate_bookstore_endpoint():
     """Tests that bookstore does not validate with an empty s3_endpoint_url."""
+    expected = {"bookstore_valid": False, "publish_valid": True, "archive_valid": True}
     settings = BookstoreSettings(s3_endpoint_url="")
-    assert not validate_bookstore(settings)['bookstore_valid']
+    assert validate_bookstore(settings) == expected
 
 
 def test_validate_bookstore_bucket():
     """Tests that bookstore does not validate with an empty s3_bucket."""
+    expected = {"bookstore_valid": True, "publish_valid": True, "archive_valid": True}
     settings = BookstoreSettings(s3_bucket="A_bucket")
-    assert validate_bookstore(settings)['bookstore_valid']
+    assert validate_bookstore(settings) == expected
