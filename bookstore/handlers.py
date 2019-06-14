0ae6def9a412bdba91722ce86bd19418cbd7dbd6
M Pacer <mpacer@berkeley.edu>
2019-06-14 15:59:07 -0700

    Add functionality to disable cloning; simplify handler settings code

--- bookstore/handlers.py
+++ bookstore/handlers.py
@@ -49,17 +49,10 @@ def load_jupyter_server_extension(nb_app):
     base_bookstore_api_pattern = url_path_join(web_app.settings['base_url'], '/api/bookstore')
     web_app.add_handlers(host_pattern, [(base_bookstore_api_pattern, BookstoreVersionHandler)])
     bookstore_settings = BookstoreSettings(parent=nb_app)
-    web_app.settings['bookstore'] = {
-        "version": version,
-        "validation": validate_bookstore(bookstore_settings),
-    }
+    validation = validate_bookstore(bookstore_settings)
+    web_app.settings['bookstore'] = {"version": version, "validation": validation}
 
-    check_published = [
-        web_app.settings['bookstore']['validation'].get("bookstore_valid"),
-        web_app.settings['bookstore']['validation'].get("publish_valid"),
-    ]
-
-    if not all(check_published):
+    if not validation['publish_valid']:
         nb_app.log.info("[bookstore] Not enabling bookstore publishing, endpoint not configured")
     else:
         nb_app.log.info(f"[bookstore] Enabling bookstore publishing, version: {version}")
@@ -73,10 +66,14 @@ def load_jupyter_server_extension(nb_app):
             ],
         )
 
-    web_app.add_handlers(
-        host_pattern,
-        [
-            (url_path_join(base_bookstore_api_pattern, r"/clone(?:/?)*"), BookstoreCloneAPIHandler),
-            (url_path_join(base_bookstore_pattern, r"/clone(?:/?)*"), BookstoreCloneHandler),
-        ],
-    )
+    if validation['cloning_valid']:
+        web_app.add_handlers(
+            host_pattern,
+            [
+                (
+                    url_path_join(base_bookstore_api_pattern, r"/clone(?:/?)*"),
+                    BookstoreCloneAPIHandler,
+                ),
+                (url_path_join(base_bookstore_pattern, r"/clone(?:/?)*"), BookstoreCloneHandler),
+            ],
+        )
