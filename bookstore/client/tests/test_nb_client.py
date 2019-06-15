e5c1bfab7162bdabc7bd9bacb07477081ead53fe
M Pacer <mpacer@berkeley.edu>
2019-06-14 19:28:08 -0700

    Test basic functionality for KernelInfo init and attribute access

---
+++ bookstore/client/tests/test_nb_client.py
@@ -0,0 +1,30 @@
+import pytest
+
+from bookstore.client.nb_client import (
+    KernelInfo,
+)
+
+
+@pytest.fixture(scope="module")
+def kernel_info_dict():
+    info_dict = {
+        "id": 'f92b7c8b-0858-4d10-903c-b0631540fb36',
+        "name": 'dev',
+        "last_activity": '2019-03-14T23:38:08.137987Z',
+        "execution_state": 'idle',
+        "connections": 0,
+    }
+    return info_dict
+
+
+@pytest.fixture(scope="module")
+def kernel_info(kernel_info_dict):
+    kernel_info = KernelInfo(**kernel_info_dict)
+    return kernel_info
+def test_kernel_info_class(kernel_info_dict, kernel_info):
+    assert kernel_info.id == kernel_info_dict['id']
+    assert kernel_info.name == kernel_info_dict["name"]
+    assert kernel_info.last_activity == kernel_info_dict["last_activity"]
+    assert kernel_info.execution_state == kernel_info_dict['execution_state']
+    assert kernel_info.connections == kernel_info_dict["connections"]
+
