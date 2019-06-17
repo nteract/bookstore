8b2b0e37b1eaf720599f486f56ead9797d70ff09
M Pacer <mpacer@berkeley.edu>
2019-06-17 10:26:56 -0700

    Add TestPublishAPIHandler testing framework

--- bookstore/tests/test_publish.py
+++ bookstore/tests/test_publish.py
@@ -1,9 +1,47 @@
 import asyncio
+import json
+
+from unittest.mock import Mock
+
 import pytest
 
 from bookstore.publish import BookstorePublishAPIHandler
+from tornado.testing import AsyncTestCase, gen_test
+from tornado.web import Application, HTTPError
+from tornado.httpserver import HTTPRequest
+from traitlets.config import Config
 
 
 def test_create_publish_handler_no_params():
     with pytest.raises(TypeError):
         assert BookstorePublishAPIHandler()
+
+
+class TestPublishAPIHandler(AsyncTestCase):
+    def setUp(self):
+        super().setUp()
+        mock_settings = {
+            "BookstoreSettings": {
+                "s3_access_key_id": "mock_id",
+                "s3_secret_access_key": "mock_access",
+            }
+        }
+        config = Config(mock_settings)
+
+        self.mock_application = Mock(spec=Application, ui_methods={}, ui_modules={}, settings={})
+
+    def put_handler(self, uri, body_dict=None, app=None):
+        if body_dict is None:
+            body_dict = {}
+        if app is None:
+            app = self.mock_application
+        connection = Mock(context=Mock(protocol="https"))
+        body = json.dumps(body_dict).encode('utf-8')
+        payload_request = HTTPRequest(
+            method='PUT',
+            uri=uri,
+            headers={"Host": "localhost:8888"},
+            body=body,
+            connection=connection,
+        )
+        return BookstorePublishAPIHandler(app, payload_request)
