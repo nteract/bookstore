2fd095db92baa2dcabdcf727c7ccbf9456268846
M Pacer <mpacer@berkeley.edu>
2019-06-27 11:10:15 -0700

    Add tests for valdiating the model directly

--- bookstore/tests/test_publish.py
+++ bookstore/tests/test_publish.py
@@ -6,6 +6,7 @@ from unittest.mock import Mock
 import pytest
 
 from bookstore.publish import BookstorePublishAPIHandler
+from nbformat.v4 import new_notebook
 from tornado.testing import AsyncTestCase, gen_test
 from tornado.web import Application, HTTPError
 from tornado.httpserver import HTTPRequest
@@ -63,14 +64,46 @@ class TestPublishAPIHandler(AsyncTestCase):
             await empty_handler.put('hi')
 
     @gen_test
-    async def test_put_bad_body(self):
-        body_dict = {'content': 2}
-        bad_body_handler = self.put_handler('/bookstore/publish/hi', body_dict=body_dict)
+    async def test_put_s3_error(self):
+        """this test includes a valid body so that we get to the s3 part of our system"""
+        body_dict = {'content': new_notebook(), 'type': "notebook"}
+        ok_body_handler = self.put_handler('/bookstore/publish/hi', body_dict=body_dict)
         with pytest.raises(HTTPError):
-            await bad_body_handler.put('hi')
+            await ok_body_handler.put('hi')
 
     def test_prepare_response(self):
         expected = {"s3path": "my_bucket/custom_prefix/mylocal/path", "versionID": "eeeeAB"}
         empty_handler = self.put_handler('/bookstore/publish/hi')
         actual = empty_handler.prepare_response("mylocal/path", {"VersionId": "eeeeAB"})
         assert actual == expected
+
+    def test_validate_model_no_type(self):
+        body_dict = {'content': {}}
+        empty_handler = self.put_handler('/bookstore/publish/hi')
+        with pytest.raises(HTTPError):
+            empty_handler.validate_model(body_dict)
+
+    def test_validate_model_wrong_type(self):
+        body_dict = {'content': {}, 'type': "file"}
+        empty_handler = self.put_handler('/bookstore/publish/hi')
+        with pytest.raises(HTTPError):
+            empty_handler.validate_model(body_dict)
+
+    def test_validate_model_empty_content(self):
+        body_dict = {'content': {}, 'type': "notebook"}
+        empty_handler = self.put_handler('/bookstore/publish/hi')
+        with pytest.raises(HTTPError):
+            empty_handler.validate_model(body_dict)
+
+    def test_validate_model_bad_notebook(self):
+        bad_notebook = new_notebook()
+        bad_notebook['other_field'] = "hello"
+        body_dict = {'content': bad_notebook, 'type': "notebook"}
+        empty_handler = self.put_handler('/bookstore/publish/hi')
+        with pytest.raises(HTTPError):
+            empty_handler.validate_model(body_dict)
+
+    def test_validate_model_good_notebook(self):
+        body_dict = {'content': new_notebook(), 'type': "notebook"}
+        empty_handler = self.put_handler('/bookstore/publish/hi')
+        empty_handler.validate_model(body_dict)
