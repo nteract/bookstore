db87ccad6a2d803a3e82daa1c5d3fdbbeceaf892
M Pacer <mpacer@berkeley.edu>
2019-06-27 17:56:47 -0700

    Add test for case where lock is already locked

--- bookstore/tests/test_archive.py
+++ bookstore/tests/test_archive.py
@@ -1,6 +1,7 @@
 """Tests for archive"""
 import asyncio
 import pytest
+import json
 
 from bookstore.archive import ArchiveRecord, BookstoreContentsArchiver
 from nbformat.v4 import new_notebook
@@ -20,10 +21,21 @@ async def test_archive_failure_on_no_lock():
     archiver = BookstoreContentsArchiver()
     assert archiver
 
-    record = ArchiveRecord('workspace/project', 'my_notebook', 100.2)
+    record = ArchiveRecord('my_notebook_path.ipynb', json.dumps(new_notebook()), 100.2)
     assert record
 
     await archiver.archive(record)
+
+
+@pytest.mark.asyncio
+async def test_archive_abort_with_lock():
+    archiver = BookstoreContentsArchiver()
+    record = ArchiveRecord('my_notebook_path.ipynb', json.dumps(new_notebook()), 100.2)
+
+    lock = asyncio.Lock()
+    archiver.path_locks['my_notebook_path.ipynb'] = lock
+    async with lock:
+        await archiver.archive(record)
 
 
 def test_pre_save_hook():
